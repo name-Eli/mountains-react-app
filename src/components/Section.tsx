943dac85de4801717dc6df648896b9964bb34e2b
Saray <sryelzn@gmail.com>
2024-02-20 18:35:30 -0500

    Create the Section component

---
+++ src/components/Section.tsx
@@ -0,0 +1,27 @@
+
+interface Props {
+    id: string;
+    prevSectionId: string;
+    nextSectionId: string;
+    photos: string[];
+
+}
+
+const Section = ({ id, prevSectionId, nextSectionId, photos }: Props) => {
+    return (
+        <section id={id}>
+            <a href={prevSectionId} className="arrow__btn left-arrow">‹</a>
+
+            {
+                photos.map((photo, index) =>
+                    <div className="item">
+                        <img key={index} src={photo} />
+                    </div>
+                )
+            }
+            <a href={nextSectionId} className="arrow__btn right-arrow">›</a>
+        </section>
+    );
+}
+
+export default Section;
